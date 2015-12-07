728a925e5da75e4d0c7d03cdbc10fd8d4a2de7a5
Kirill Novik <kino6052@colorado.edu>
2015-12-07 16:38:24 -0700

    Update script.js

--- www/js/script.js
+++ www/js/script.js
@@ -1,19 +1,45 @@
 $(function(){
+	function GetURLParameter(sParam)
+	{
+	    var sPageURL = window.location.search.substring(1);
+	    var sURLVariables = sPageURL.split('&');
+	    for (var i = 0; i < sURLVariables.length; i++)
+	    {
+	        var sParameterName = sURLVariables[i].split('=');
+	        if (sParameterName[0] == sParam)
+	        {
+	        	console.log(sParameterName);
+	            return sParameterName[1];
+	        }
+	    }
+	}
+
 	$("#getdata").click(function(){
-		window.location = "https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=229WMB&redirect_uri=http%3A%2F%2F192.168.1.24/data.html&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight"
+		alert("test");
+		var url = {
+			response_type: "response_type=code&",
+			client_id: "client_id=229WMB&",
+			redirect_uri: "redirect_uri=" + encodeURIComponent("https://feedmewell-kino6052.c9.io/csci-3308-Project-GroupAwesome/www/data.html") + "&",
+			scope: "scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight"
+		};
+		window.location = "https://www.fitbit.com/oauth2/authorize?" + url.response_type + url.client_id + url.redirect_uri + url.scope;
 	});
+	
 	$("#button").click(function(){
-		$("#button").text("Click...");
-		var location = String(window.location);
-		var code = location.substr(35, location.length-1);
+		console.log(GetURLParameter("code"));
 		$.ajax({
-			url: "https://api.fitbit.com/oauth2/token?client_id=229WMBC&grant_type=authorization_code&redirect_uri=http%3A%2F%2F192.168.1.24/data.html&code=" + code,
+			url: "https://api.fitbit.com/oauth2/token",
+			data: {
+				client_id:"229WMB",
+				grant_type: "authorization_code",
+				redirect_uri: "https://feedmewell-kino6052.c9.io/csci-3308-Project-GroupAwesome/www/data.html",
+				code: GetURLParameter("code")
+			},
 			type: 'POST',
 			headers: {
 				"Authorization": "Basic " + window.btoa("229WMB:e10e600a259dc0696dd9220bde092c7e"),
 				"Content-Type": "application/x-www-form-urlencoded"
 			},
-			
 			success: function(response){
 				$.ajax({
 					url: "https://api.fitbit.com/1/user/-/profile.json",
